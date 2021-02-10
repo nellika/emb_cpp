59154c2c88acaf5adaadb488d3240ae5f3937795
nellika <nynelli@yahoo.de>
2021-02-10 17:57:26 +0100

    more structured class design

--- 02/3_Class_Design/elec4_util.h
+++ 02/3_Class_Design/elec4_util.h
@@ -14,20 +14,26 @@ class LinearInterpolation {
     std::vector<double> _as;
     size_t _size;
 
+    void calculateSlope() {
+        double a;
+        for (size_t i = 0; i < _size; i++) {
+            // store slope coeffs into priv member variable
+            a = (_ys[i+1] - _ys[i])/(_xs[i+1]- _xs[i]);
+            _as.push_back(a);
+            // std::cout << a << std::endl;
+        }
+    }
+
  public:
-    LinearInterpolation(std::vector<double> xs, std::vector<double> ys) : _xs{xs}, _ys{ys} {
+    LinearInterpolation(std::vector<double> xs,
+                        std::vector<double> ys) : _xs{xs}, _ys{ys} {
+        // initialize priv member variables
         _size = _xs.size();
 
         _xs.push_back(1.0);
         _ys.push_back(_ys[0]);
 
-        double a;
-        for (size_t i = 0; i < _size; i++) {
-            // store slope coeffs into member variable
-            a = (_ys[i+1] - _ys[i])/(_xs[i+1]- _xs[i]);
-            _as.push_back(a);
-            // std::cout << a << std::endl;
-        }
+        calculateSlope();
     }
 
     double get_value(double x) const {
@@ -48,7 +54,7 @@ class Spline {
     Eigen::MatrixXd _M;
     Eigen::VectorXd _v, _u;  // v for delta ys, u for the unknowns
 
-    void _calcNoDeriv(size_t n, size_t band, double delta) {
+    void calcNoDerivAndColVector(size_t n, size_t band, double delta) {
         // polynomials >> first row
         for (size_t i = 0; i < 3; i++) {
             _M(band, band + i) = pow(delta, 3-i);
@@ -56,20 +62,12 @@ class Spline {
         _v(band) = _ys[n + 1] - _ys[n];
     }
 
- public:
-    Spline(std::vector<double> xs, std::vector<double> ys) : _xs{xs}, _ys{ys} {
-        _size = _xs.size();
-        _xs.push_back(1.0);
-        _ys.push_back(_ys[0]);
-
-        _M = Eigen::MatrixXd::Zero(_size*3, _size*3);
-        _v = Eigen::VectorXd::Zero(_size*3);
-
+    void solveLPwithConditions() {
         int16_t band = 0;
         double delta = _xs[1] - _xs[0];
 
         for (size_t i = 0; i < _size-1; i++) {
-            _calcNoDeriv(i, band, delta);
+            calcNoDerivAndColVector(i, band, delta);
 
             // first derivative >> second row
             _M(band + 1, band + 0) = 3 * delta * delta;
@@ -88,8 +86,7 @@ class Spline {
             // std::cout << _M << std::endl;
         }
 
-
-        _calcNoDeriv(_size-1, band, delta);
+        calcNoDerivAndColVector(_size-1, band, delta);
 
         // null tangents
         _M(band + 1,  2) = 1.0;
@@ -98,18 +95,34 @@ class Spline {
         _M(band + 2,  band + 1) = 2 * delta;
         _M(band + 2,  band + 2) = 1.0;
 
-        // solving the matrix
+        // solving the LP
         _u = _M.fullPivHouseholderQr().solve(_v);
+    }
 
-        // get coefficients
+    void buildCoeffVectors() {
+        int16_t band;
         for (size_t i = 0; i < _size; ++i) {
             band = 3 * i;
             _as.push_back(_u[band + 0]);
             _bs.push_back(_u[band + 1]);
             _cs.push_back(_u[band + 2]);
         }
+    }
 
-        // std::cout << _u << std::endl;
+ public:
+    Spline(std::vector<double> xs, std::vector<double> ys) : _xs{xs}, _ys{ys} {
+        // initialize member variables
+        _size = _xs.size();
+        _xs.push_back(1.0);
+        _ys.push_back(_ys[0]);
+
+        _M = Eigen::MatrixXd::Zero(_size*3, _size*3);
+        _v = Eigen::VectorXd::Zero(_size*3);
+
+        // solve linear problem
+        solveLPwithConditions();
+        // get coefficients
+        buildCoeffVectors();
     }
 
     double get_value(double x) const {
@@ -125,5 +138,7 @@ class Spline {
                _cs[idx] * delta +
                _ys[idx];
     }
+
+    Eigen::MatrixXd get_matrix() const { return _M;}
 };
-}
+}   // namespace ELEC4
